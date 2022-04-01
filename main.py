9b86435f6ec8acc90019aefecd43c2020da16936
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-31 21:43:42 -0300

    Started another example using Kivy

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
+
+Window.size = (480, 720)
+
+
+class MainInterface(Widget):
+    pass
+
+
+class CustomApp(App):
+    pass
+
+
+CustomApp().run()
