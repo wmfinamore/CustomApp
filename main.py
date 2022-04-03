e9fa88f76b60b014ded2a19b6ad5da912e37965d
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-02 22:41:06 -0300

    Implemented transition between screens with screenmanager

--- main.py
+++ main.py
@@ -8,20 +8,31 @@ Window.size = (480, 720)
 
 class MainInterface(Widget):
 
-    def S_pressing(self, s_img):
+    def S_pressing(self, s_img, SM):
         s_img.source = "Pressed_Start_Btn.png"
+        if SM.current == "background_2" or SM.current == "background_3":
+            SM.transition.direction = "right"
+        SM.current = "background_1"
 
     def S_releasing(self, s_img):
         s_img.source = "Start_Btn.png"
 
-    def ST_pressing(self, st_img):
+    def ST_pressing(self, st_img, SM):
         st_img.source = "Pressed_Stats_Btn.png"
+        if SM.current == "background_1":
+            SM.transition.direction = "left"
+        if SM.current == "background_3":
+            SM.transition.direction = "right"
+        SM.current = "background_2"
 
     def ST_releasing(self, st_img):
         st_img.source = "Stats_Btn.png"
 
-    def L_pressing(self, l_img):
+    def L_pressing(self, l_img, SM):
         l_img.source = "Pressed_Likes_Btn.png"
+        if SM.current == "background_1" or SM.current == "background_2":
+            SM.transition.direction = "left"
+        SM.current = "background_3"
 
     def L_releasing(self, l_img):
         l_img.source = "Likes_Btn.png"
