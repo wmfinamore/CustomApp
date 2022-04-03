c9d4eb80cc533c9d65206185771e9c71b14a07d7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-02 22:28:49 -0300

    Implemented animation in the app buttons

--- main.py
+++ main.py
@@ -7,7 +7,24 @@ Window.size = (480, 720)
 
 
 class MainInterface(Widget):
-    pass
+
+    def S_pressing(self, s_img):
+        s_img.source = "Pressed_Start_Btn.png"
+
+    def S_releasing(self, s_img):
+        s_img.source = "Start_Btn.png"
+
+    def ST_pressing(self, st_img):
+        st_img.source = "Pressed_Stats_Btn.png"
+
+    def ST_releasing(self, st_img):
+        st_img.source = "Stats_Btn.png"
+
+    def L_pressing(self, l_img):
+        l_img.source = "Pressed_Likes_Btn.png"
+
+    def L_releasing(self, l_img):
+        l_img.source = "Likes_Btn.png"
 
 
 class CustomApp(App):
